e3484d2fbf5bcae244b6604e91d5eba07bb624fc
Dominic <dgozza@peakactivity.com>
2020-02-10 13:38:40 -0500

    moved js

--- scene.js
+++ scene.js
@@ -1,19 +1,4 @@
-(() => {
-    console.log("HERE")
-    const modelViewer = document.querySelector('#lamp');
-    const orbitCycle = [
-      '45deg 55deg 4m',
-      '-60deg 110deg 2m',
-      modelViewer.cameraOrbit
-    ];
-    console.log(modelViewer.getFieldOfView, "???")
-  
-    setInterval(() => {
-      const currentOrbitIndex = orbitCycle.indexOf(modelViewer.cameraOrbit);
-      modelViewer.cameraOrbit =
-          orbitCycle[(currentOrbitIndex + 1) % orbitCycle.length];
-    }, 3000);
-  })();
+
 
 
 
